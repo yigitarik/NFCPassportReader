6af8df563e0768461911b1b42f55f1ad180932d4
Yiğit ARİK <63970971+yigitarik@users.noreply.github.com>
2026-03-02 11:39:11 +0300

    Update NFCViewDisplayMessage.swift

--- Sources/NFCPassportReader/NFCViewDisplayMessage.swift
+++ Sources/NFCPassportReader/NFCViewDisplayMessage.swift
@@ -21,33 +21,43 @@ public enum NFCViewDisplayMessage {
 extension NFCViewDisplayMessage {
     public var description: String {
         switch self {
-            case .requestPresentPassport:
-                return "Hold your iPhone near an NFC enabled passport."
-            case .authenticatingWithPassport(let progress):
-                let progressString = handleProgress(percentualProgress: progress)
-                return "Authenticating with passport.....\n\n\(progressString)"
-            case .readingDataGroupProgress(let dataGroup, let progress):
-                let progressString = handleProgress(percentualProgress: progress)
-                return "Reading \(dataGroup).....\n\n\(progressString)"
-            case .error(let tagError):
-                switch tagError {
-                    case NFCPassportReaderError.TagNotValid:
-                        return "Tag not valid."
-                    case NFCPassportReaderError.MoreThanOneTagFound:
-                        return "More than 1 tags was found. Please present only 1 tag."
-                    case NFCPassportReaderError.ConnectionError:
-                        return "Connection error. Please try again."
-                    case NFCPassportReaderError.InvalidMRZKey:
-                        return "MRZ Key not valid for this document."
-                    case NFCPassportReaderError.ResponseError(let description, let sw1, let sw2):
-                        return "Sorry, there was a problem reading the passport. \(description) - (0x\(sw1), 0x\(sw2)"
-                    default:
-                        return "Sorry, there was a problem reading the passport. Please try again"
-                }
-            case .activeAuthentication:
-                return "Authenticating....."
-            case .successfulRead:
-                return "Passport read successfully"
+        case .requestPresentPassport:
+            return "Kimliği telefonun arkasına yaklaştırın."
+
+        case .authenticatingWithPassport(let progress):
+            let progressString = handleProgress(percentualProgress: progress)
+            return "Kimlik doğrulanıyor...\n\n\(progressString)"
+
+        case .readingDataGroupProgress(_, let progress):
+            let progressString = handleProgress(percentualProgress: progress)
+            return "Kimlik okunuyor...\n\n%\(progressString)"
+
+        case .error(let tagError):
+            switch tagError {
+            case NFCPassportReaderError.TagNotValid:
+                return "Kimlik etiketi geçerli değil."
+
+            case NFCPassportReaderError.MoreThanOneTagFound:
+                return "Birden fazla kimlik algılandı. Lütfen yalnızca bir kimlik gösterin."
+
+            case NFCPassportReaderError.ConnectionError:
+                return "Bağlantı hatası. Lütfen tekrar deneyin."
+
+            case NFCPassportReaderError.InvalidMRZKey:
+                return "Bu kimlik için güvelik anahtarı geçerli değil."
+
+            case NFCPassportReaderError.ResponseError(let description, let sw1, let sw2):
+                return "Kimlik okunurken bir sorun oluştu. Lütfen tekrar deneyin."
+
+            default:
+                return "Kimlik okunurken bir hata oluştu. Lütfen tekrar deneyin."
+            }
+
+        case .activeAuthentication:
+            return "Doğrulama yapılıyor..."
+
+        case .successfulRead:
+            return "Kimlik başarıyla okundu."
         }
     }
     
